19ff7ad7b7d48937b823797fb6d4c1f6562e1999
Lenon Corrêa <lenonac_@hotmail.com>
2020-11-27 15:25:08 -0300

    Projeto Final

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AuthguardService } from './auth/guards/authguard.service';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthguardService] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,36 @@
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AuthguardService } from './auth/guards/authguard.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './pages/home/home.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { AlertComponent } from './components/alert/alert/alert.component';
+import { DestaqueDirective } from './diretivas/destaque.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ModalComponent,
+    AlertComponent,
+    DestaqueDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [AuthguardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
